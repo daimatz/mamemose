05d89aaf6abb94b2adf9d03511251494262bd6be
daimatz <dai@daimatz.net>
2012-12-24 23:36:06 +0900

    table support

--- lib/mamemose.rb
+++ lib/mamemose.rb
@@ -169,6 +169,40 @@ hr {
     border: none;
     border-top: 1px solid black;
 }
+table {
+    padding: 0;
+    margin: 1em 2em;
+    border-spacing: 0;
+    border-collapse: collapse;
+}
+table tr {
+    border-top: 1px solid #cccccc;
+    background-color: white;
+    margin: 0;
+    padding: 0;
+}
+table tr:nth-child(2n) {
+    background-color: #f8f8f8;
+}
+table tr th {
+    font-weight: bold;
+    border: 1px solid #cccccc;
+    text-align: left;
+    margin: 0;
+    padding: 6px 13px;
+}
+table tr td {
+    border: 1px solid #cccccc;
+    text-align: left;
+    margin: 0;
+    padding: 6px 13px;
+}
+table tr th :first-child, table tr td :first-child {
+    margin-top: 0;
+}
+table tr th :last-child, table tr td :last-child {
+    margin-bottom: 0;
+}
 --></style>
 <script>
 function copy(text) {
@@ -422,7 +456,8 @@ HTML
     markdown = Redcarpet::Markdown.new(HTMLwithSyntaxHighlighter,
                                        {:strikethrough => true,
                                          :autolink => true,
-                                         :fenced_code_blocks => true})
+                                         :fenced_code_blocks => true,
+                                         :tables => true})
     markdown.render(text)
   end
 end
--- lib/mamemose/version.rb
+++ lib/mamemose/version.rb
@@ -1,3 +1,3 @@
 module Mamemose
-  VERSION = "0.2.0.1"
+  VERSION = "0.2.1"
 end
