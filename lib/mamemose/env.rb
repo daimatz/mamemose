ba6f4c25b4e042b0f316f24d0fe834f21da49a54
daimatz <dai@daimatz.net>
2013-04-20 02:32:39 +0900

    update IGNORE_FILES default value

--- lib/mamemose/env.rb
+++ lib/mamemose/env.rb
@@ -7,9 +7,7 @@ PORT = 20000 if !defined?(PORT)
 WS_PORT = 20001 if !defined?(WS_PORT)
 RECENT_NUM = 10 if !defined?(RECENT_NUM)
 RECENT_PATTERN = /.*/ if !defined?(RECENT_PATTERN)
-IGNORE_FILES = ['.DS_Store','.AppleDouble','.LSOverride','Icon',/^\./,/~$/,
-                '.Spotlight-V100','.Trashes','Thumbs.db','ehthumbs.db',
-                'Desktop.ini','$RECYCLE.BIN',/^#/,'MathJax','syntaxhighlighter'] if !defined?(IGNORE_FILES)
+IGNORE_FILES = [/^\./,/^#/,/~$/,'Icon','Thumbs.db','ehthumbs.db', 'Desktop.ini','$RECYCLE.BIN','hidden'] if !defined?(IGNORE_FILES)
 MARKDOWN_PATTERN = /\.(md|markdown)$/ if !defined?(MARKDOWN_PATTERN)
 INDEX_PATTERN = /^README/i if !defined?(INDEX_PATTERN)
 CUSTOM_HEADER = '' if !defined?(CUSTOM_HEADER)
